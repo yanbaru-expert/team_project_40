2ac5f038291aff308220a02679ea4890939a2927
takashi <mr.takamuu@gmail.com>
2021-01-18 18:55:00 +0900

    rails g active_admin:installを実行

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@ PASSWORD = 'password'
 User.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORDputs 'ユーザーの初期データインポートに成功しました。'
 end
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
