143584ae13f058e74f5b266f60b574a4350f0025
新田陸人 <>
2021-01-28 19:11:25 +0900

    @movies.offset_valueを使用する形に修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,5 @@ class MoviesController < ApplicationController
 
     def index
         @movies = Movie.page(params[:page]).per(PER_PAGE)
-        @base_level = Movie.base_level(params[:page])
     end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,8 +2,4 @@ class Movie < ApplicationRecord
   validates :genre, presence: true
   validates :title, presence: true
   validates :url, presence: true
-
-  def self.base_level(page)
-    (self.page(page).current_page - 1) * 10
-  end
 end
