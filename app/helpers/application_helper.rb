a8375bb7c48d2ad7c97524b830dca453c12b91f4
新田陸人 <>
2021-02-04 19:14:38 +0900

    ヘルパーメソッドを定義

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def max_width
     devise_controller? ? "mw-md" : "mw-xl"
   end
+
+  def color
+    if controller.controller_name == "lines"
+      "bg-success"
+    else
+      "bg-primary"
+    end
+  end
 end
