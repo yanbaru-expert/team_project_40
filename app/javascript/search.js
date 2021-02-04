5333d42ee639bb8a7656d5849b355e2814e90258
takashi <mr.takamuu@gmail.com>
2021-02-04 11:54:44 +0900

    見た目の修正とそれに伴うsearch.jsの調整

--- app/javascript/search.js
+++ app/javascript/search.js
@@ -11,18 +11,19 @@ $(function () {
     })
     .done(function(data) {
       // console.log(data);
-      $('.js-texts div').remove();
+      $('.row div').remove();
 
       $(data).each(function(i,text) {
-        $('.js-texts').append(
+        $('.row').append(
           `<div class="col-12 col-md-6 col-lg-4 text-card-container">
             <div class="card content-card border-dark mb-3">
-              <img class="card-img-top" src="https://d5izmuz0mcjzz.cloudfront.net/texts/no_image.jpg" width="100%">
+              <a target="_blank" rel="noopenernoreferrer" href="/texts/${text.id}"></a>
+                <img class="card-img-top" src="https://d5izmuz0mcjzz.cloudfront.net/texts/no_image.jpg" width="100%">
                 <div class="card-body text-dark text-card-body">
                   <p class="card-text text-title">${text.title}</p>
                   <p>【${text.genre}】</p>
-                  <a href="/texts/${text.id}">${text.title}</a>
                 </div>
+              </a>
             </div>
           </div>`
         );
