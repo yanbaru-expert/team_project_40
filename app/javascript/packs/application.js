769083cb31dc52039f32d06b8d6b2dff82b5c0a6
新田陸人 <>
2021-02-09 19:00:48 +0900

    application.js の記述をcalendar.js ファイルに切り出し

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,25 +3,4 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("bootstrap/dist/js/bootstrap")
-
-import { Calendar } from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import listPlugin from '@fullcalendar/list';
-import bootstrapPlugin from '@fullcalendar/bootstrap';
-
-document.addEventListener('turbolinks:load', function() {
-  var calendarEl = document.getElementById('calendar');
-  var calendar = new Calendar(calendarEl, {
-    plugins: [ dayGridPlugin, timeGridPlugin, listPlugin, bootstrapPlugin ],
-    initialView: 'dayGridMonth',
-    themeSystem: 'bootstrap',
-    headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,listWeek'
-    }
-  });
-
-  calendar.render();
-});
+require("../calendar")
