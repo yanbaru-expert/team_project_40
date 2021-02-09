60bb0bd27cdb1a11e0cead99d918ccb4c3d26669
takashi <mr.takamuu@gmail.com>
2021-02-09 17:36:19 +0900

    Model作成(read.rb)と多対多の関連付け、日本語対応

--- app/models/text.rb
+++ app/models/text.rb
@@ -1,4 +1,8 @@
 class Text < ApplicationRecord
+  belongs_to :user
+  has_many :reads, dependent: :destroy
+  # text.read_users で text を「読破」しているユーザーの一覧を取得できるようになる
+  has_many :read_users, through: :reads, source: :user
   validates :genre, presence: true
   validates :content, presence: true
   validates :title, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :reads, dependent: :destroy
+  # user.read_texts で user が「読破」しているメッセージの一覧を取得できるようになる
+  has_many :read_texts, through: :reads, source: :text
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
