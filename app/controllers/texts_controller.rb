7acbdaabf48110a84c15b17221baf6d4c908b772
takashi <mr.takamuu@gmail.com>
2021-02-11 14:06:37 +0900

    部分テンプレート作成とN＋１問題対応

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,6 +1,7 @@
 class TextsController < ApplicationController
   def index
     @texts = Text.where(genre: ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]).order(:genre)
+    @read_text_ids = current_user.reads.pluck(:text_id) 
     @text = Text.find_by(params[:id])
   end
 
