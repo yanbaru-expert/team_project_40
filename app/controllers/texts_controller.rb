ce988b57962af58572dcab40ac0ef5a576fdde59
takashi <mr.takamuu@gmail.com>
2021-02-16 17:30:08 +0900

    不要コードの削除、詳細ページへのリンク追加

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -2,7 +2,6 @@ class TextsController < ApplicationController
   def index
     @texts = Text.where(genre: ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]).order(:id)
     @read_text_ids = current_user.reads.pluck(:text_id) 
-    @text = Text.find_by(params[:id])
   end
 
   def show
