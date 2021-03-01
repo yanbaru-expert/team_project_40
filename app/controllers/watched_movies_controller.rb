5c70dff179b4a5fb67b12fc943a0fc9be5d519ff
takashi <mr.takamuu@gmail.com>
2021-03-01 11:12:43 +0900

    コントローラー修正（watched_movies_controller）

--- app/controllers/watched_movies_controller.rb
+++ app/controllers/watched_movies_controller.rb
@@ -1,11 +1,11 @@
-class WatchedMoviesControllers < ApplicationController
+class WatchedMoviesController < ApplicationController
   def create
-    current_user.watched_movies.create!(watched_movie_id: params[:watched_movie_id])
+    current_user.watched_movies.create!(movie_id: params[:movie_id])
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    current_user.watched_movies.find_by(watched_movie_id: params[:watched_movie_id]).destroy!
+    current_user.watched_movies.find_by(movie_id: params[:movie_id]).destroy!
     redirect_back(fallback_location: root_path)
   end
 end
