92aa4858013abd3950986e80165633a43dd251a3
takashi <mr.takamuu@gmail.com>
2021-03-01 11:53:00 +0900

    N+1問題対処(watched_moviesテーブルから一度にmovie_idを取得をコントローラー追加)

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,8 @@
 class MoviesController < ApplicationController
-    PER_PAGE = 10
+  PER_PAGE = 10
 
-    def index
-        @movies = Movie.page(params[:page]).per(PER_PAGE)
-    end
+  def index
+    @movies = Movie.page(params[:page]).per(PER_PAGE)
+    @watched_movie_ids = current_user.watched_movies.pluck(:movie_id)
+  end
 end
