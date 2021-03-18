94142b6e976c665c26c4b506922adc02821b881e
kazu <skaz0110@yahoo.co.jp>
2021-03-18 21:51:30 +0900

    コントローラーで必要なデータを取得するように修正

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,9 +1,18 @@
 class MypagesController < ApplicationController
   def index
-    @texts = Text.all
+    
+    @genreCount =
+    {
+      userBasic: current_user.read_texts.where(genre:"Basic").count,
+      allBasic:  Text.all.where(genre:"Basic").count,
+      userGit:   current_user.read_texts.where(genre:"Git").count,
+      allGit:    Text.all.where(genre:"Git").count,
+      userRuby:  current_user.read_texts.where(genre:"Ruby").count,
+      allRuby:   Text.all.where(genre:"Ruby").count,
+      userRails: current_user.read_texts.where(genre:"Ruby on Rails").count,
+      allRails:  Text.all.where(genre:"Ruby on Rails").count,
+    }
 
-    # ログインユーザーの「読破済み」を取得
-    @userReadText = current_user.read_texts
   end
   
 end
